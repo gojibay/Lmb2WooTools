2ed6df4b4cdd57789ec62f64fcf69d0ad32cb336
LG <lgp@hibox.fr>
2017-04-17 11:46:07 +0200

    add comments, usage and output messages

--- check_size_and_resize.sh
+++ check_size_and_resize.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# usage :
+# ./check_size_and_resize.sh image_file_name
+# example : 
+# for i in * ; do if [ -f $i ] ; then echo $i ; ./check_size_and_resize.sh $i  ; fi; done
+
 # initializations
-size=2048
-filename=$1
-target_dir='target'
+size=2048 			# this is the size images will be resized to
+filename=$1  		# get filename from first command line argument
+target_dir='target'	# target directory resized images will be written to
 
 # infos on images
 width=`identify -format "%w"  $filename`
@@ -14,6 +19,7 @@ echo "#################"
 echo "Widht : " $width
 echo "Height : " $height
 
+# mogrify overwrites input image file
 # mogrify -verbose -resize "500<x500<" $i -gravity center  -background white -flatten ${target_dir}/$i  
 
 echo "Resizing started..."
@@ -43,7 +49,8 @@ elif [ $width -lt $size ] ; then
                 identify ${target_dir}/$filename
 
         fi
-	
+echo "...resizing done."
+echo ""
 
 fi
 
